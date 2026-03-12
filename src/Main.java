134117610c08e524909d51ac3dce299c682eb643
Dheerajparihar111 <parihardheeraj.p@gmail.com>
2026-03-12 23:42:20 +0530

    added menu options

--- src/Main.java
+++ src/Main.java
@@ -6,23 +6,36 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
+        System.out.println("===========Wel-come to EMI Calculator===========");
+        System.out.println("______________________");
+        System.out.println("| 1) EMI Calculating |");
+        System.out.println("| 2) EXIT            |");
+        System.out.println("----------------------");
+        System.out.print("Enter Your Choice :");
+        int choice = input.nextInt();
+        switch (choice) {
+            case 1 :
+            System.out.println("Enter the principal:");
+            double p = input.nextDouble();
 
-        System.out.println("Enter the principal:");
-        double p = input.nextDouble();
+            System.out.println("Enter the Annual Interest Rate:");
+            float R = input.nextFloat();
 
-        System.out.println("Enter the Annual Interest Rate:");
-        float R = input.nextFloat();
+            System.out.println("Enter the Months:");
+            int Months = input.nextInt();
 
-        System.out.println("Enter the Months:");
-        int Months = input.nextInt();
+            double rate = R / (12 * 100);
 
-        double rate = R / (12 * 100);
+            double EMI = (p * rate * Math.pow(1 + rate, Months)) /
+                    (Math.pow(1 + rate, Months) - 1);
 
-        double EMI = (p * rate * Math.pow(1 + rate, Months)) /
-                (Math.pow(1 + rate, Months) - 1);
+            System.out.println("Monthly EMI = " + EMI);
+           break;
 
-        System.out.println("Monthly EMI = " + EMI);
-
+            case 2:
+                System.out.println("Thank You For Using EMI Calculator");
+                System.exit(0);
+        }
         input.close();
     }
 }
